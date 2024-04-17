54ee79617f0b6a036469331f661c2afbd3f7776e
avicity7 <karlorjalo@gmail.com>
2024-04-17 13:05:45 +0800

    fix: Add gitignore on the backend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import BusCard from "../components/busCard.svelte";
   let bus_list = [{bus_id: 1, carplate: "S123456789Z", status: 1, route: "East Route"}, {bus_id: 2, carplate: "S987654321Z", status: 1, route: "West Route"}]
+
+  let number = 0
 </script>
 
 <div class="p-6 md:p-12">
