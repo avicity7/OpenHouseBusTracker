af4fe0b8f2ddea485a26100b0f58400b6df915e2
KohHanMing123 <kohhanming123@gmail.com>
2024-05-07 15:12:58 +0800

    feat: cache schedule BE + used icons for schedule admin table

--- frontend/src/routes/admin/schedule/+page.svelte
+++ frontend/src/routes/admin/schedule/+page.svelte
@@ -194,7 +194,7 @@
             <tbody class="bg-white divide-y divide-gray-200">
                 {#if filteredSchedules.length > 0}
                     {#each filteredSchedules as schedule (schedule.BusScheduleId)}
-                        <tr>
+                        <tr class="hover:bg-gray-100">
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <input type="checkbox" checked={selectedSchedules.has(schedule.BusScheduleId)} on:change={() => {
                                     toggleSelection(schedule.BusScheduleId);
@@ -219,7 +219,7 @@
                     </tr>
                 {:else}
                     {#each busSchedule as schedule (schedule.BusScheduleId)}
-                        <tr>
+                        <tr class="hover:bg-gray-100">
                             <td class="px-6 py-4 whitespace-nowrap">
                                 <input type="checkbox" checked={selectedSchedules.has(schedule.BusScheduleId)} on:change={() => {
                                     toggleSelection(schedule.BusScheduleId);
@@ -233,9 +233,25 @@
                             <td class="px-6 py-4 whitespace-nowrap">{formatTimestamp(schedule.StartTime)}</td>
                             <td class="px-6 py-4 whitespace-nowrap">{formatTimestamp(schedule.EndTime)}</td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
-                                <a href={`schedule/update-schedule/${schedule.BusScheduleId}`} class="text-green-600 hover:text-green-900 mr-5">Update</a>
-                                <button class="text-red-600 hover:text-red-900" on:click={() => deleteSchedule(schedule.BusScheduleId)}>Delete</button>
+                                <div class="flex items-center">
+                                    <a href={`schedule/update-schedule/${schedule.BusScheduleId}`} class="text-slate-500 hover:text-green-500 mr-8">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                            <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
+                                        </svg>
+                                    </a>
+                                    <button class="text-slate-500 hover:text-red-600 text-2xl" on:click={() => deleteSchedule(schedule.BusScheduleId)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
+                                                <g fill="none">
+                                                    <path d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                                    <path fill="currentColor" d="M14.28 2a2 2 0 0 1 1.897 1.368L16.72 5H20a1 1 0 1 1 0 2l-.003.071l-.867 12.143A3 3 0 0 1 16.138 22H7.862a3 3 0 0 1-2.992-2.786L4.003 7.07A1.01 1.01 0 0 1 4 7a1 1 0 0 1 0-2h3.28l.543-1.632A2 2 0 0 1 9.721 2zm3.717 5H6.003l.862 12.071a1 1 0 0 0 .997.929h8.276a1 1 0 0 0 .997-.929zM10 10a1 1 0 0 1 .993.883L11 11v5a1 1 0 0 1-1.993.117L9 16v-5a1 1 0 0 1 1-1m4 0a1 1 0 0 1 1 1v5a1 1 0 1 1-2 0v-5a1 1 0 0 1 1-1m.28-6H9.72l-.333 1h5.226z" />
+                                                </g>
+                                            </svg>
+                                        </svg>
+                                    </button>
+                                </div>
                             </td>
+                            
                         </tr>
                     {/each}
                 {/if}
