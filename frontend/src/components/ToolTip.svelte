36f4fb9d80edd9f2d4bcee9b519dfac51d1bb245
KohHanMing123 <kohhanming123@gmail.com>
2024-05-08 17:29:22 +0800

    feat: ToolTip component for all buttons

---
+++ frontend/src/components/ToolTip.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let text = '';
+  
+    let isVisible = false;
+  
+    const handleMouseEnter = () => {
+      isVisible = true;
+    };
+  
+    const handleMouseLeave = () => {
+      isVisible = false;
+    };
+  </script>
+  
+  <span
+    on:mouseenter={handleMouseEnter}
+    on:mouseleave={handleMouseLeave}
+    class="relative inline-block"
+    role="tooltip"
+  >
+    <slot />
+  
+    {#if isVisible}
+    <div class="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 px-4 py-2 bg-gray-800 text-white text-sm rounded-lg">
+        {text}
+      </div>
+    {/if}
+  </span>
+  
