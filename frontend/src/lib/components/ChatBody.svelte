46388509f72b7aec056753ea059b0c2ba1ee7fc8
avicity7 <74177090+avicity7@users.noreply.github.com>
2024-07-08 20:26:32 +0800

    feat: Dashboard UI changes, Chat UI changes, Password Reset

--- frontend/src/lib/components/ChatBody.svelte
+++ frontend/src/lib/components/ChatBody.svelte
@@ -6,7 +6,7 @@
   const { account } = data
 </script>
 
-<div class={`w-[80vw] flex flex-row ${message.From == account?.Email ? "justify-end" : "justify-start"}`}>
+<div class={`ml-20 w-[70vw] flex flex-row ${message.From == account?.Email ? "justify-end" : "justify-start"}`}>
   <div class={`rounded-md p-4 mb-2 ${message.From == account?.Email ? "bg-red-100" : "bg-gray-100"}`}>
     {message.Body}
   </div>
