5af7ca12f7e1b5e1df77db899b817454697a8811
Karl <karlorjalo@gmail.com>
2024-07-16 23:15:26 +0800

    feat: Edit Bus Name/Carplate

--- backend/routes/bus.go
+++ backend/routes/bus.go
@@ -13,6 +13,7 @@ func Bus(r chi.Router) {
 		r.Get("/get-buses", http.HandlerFunc(controllers.GetBuses))
 		r.Get("/get-bus-status/{bus_id}", http.HandlerFunc(controllers.GetBusStatus))
 		r.Put("/update-bus-visibility/{bus_id}/{hidden}", http.HandlerFunc(controllers.UpdateBusVisibility))
+		r.Put("/update-bus/{bus_id}/{carplate}", http.HandlerFunc(controllers.UpdateBus))
 		r.Delete("/delete-bus/{bus_id}", http.HandlerFunc(controllers.DeleteBus))
 	})
 }
