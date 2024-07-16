a08de8ea8a1e1197b746a3edb240039e409ef684
KohHanMing123 <kohhanming123@gmail.com>
2024-07-16 17:45:09 +0800

    feat: edit bus name FE and half BE

--- frontend/src/routes/(admin)/admin/buses/+page.svelte
+++ frontend/src/routes/(admin)/admin/buses/+page.svelte
@@ -189,6 +189,13 @@
                                       {/if}
                                     </ToolTip>
                                 </button>
+                                <a href={`buses/update-bus/${encodeURIComponent(JSON.stringify(bus))}`} class="text-stone-500 hover:text-green-500 mr-8">
+                                    <ToolTip text="Update Bus"> 
+                                      <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                        <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
+                                      </svg>
+                                    </ToolTip>
+                                </a>
                                 <button class="text-stone-500 hover:text-red-600 text-2xl" on:click={() => deleteBus(bus.Carplate)}>
                                     <ToolTip text="Delete Bus"> 
                                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...$$props}>
