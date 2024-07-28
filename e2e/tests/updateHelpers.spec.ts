c1c9bfada59819d28ea88cb84d47b2bab823ba47
lalibiluo <lalibiluo@gmail.com>
2024-07-28 21:06:31 +0800

    feat: reassign bus and drivers to students

---
+++ e2e/tests/updateHelpers.spec.ts
@@ -0,0 +1,45 @@
+import { test, expect } from '@playwright/test';
+import SignInPage from '../models/SignInPage';
+
+test.describe('Reassign Bus and Drivers', () => {
+    test('Update Student Helpers', async ({ page }) => {
+        //Login
+        let signInPage = new SignInPage(page);
+        await signInPage.visit();
+        const email = 'sarahaxl7@gmail.com'; 
+        const password = '123'; 
+        console.log('Attempting to sign in with provided credentials');
+        await signInPage.signIn(email, password);
+        const currentURL = page.url();
+        console.log('Current URL after login:', currentURL);
+        if (!currentURL.includes('/admin/event-helper')) {
+            console.log('Navigating manually to /admin/event-helper');
+            await page.goto('http://open-house-bus-tracker.vercel.app/admin/event-helper');
+        }
+        const finalURL = page.url();
+        expect(finalURL).toContain('/admin/event-helper');
+        await page.screenshot({ path: 'admin-event-helper-page.png' });
+
+        //Update Helper Page
+        const addScheduleButton = await page.locator('[data-testid="update-helper"]');
+        await addScheduleButton.click();
+        await page.goto('https://open-house-bus-tracker.vercel.app/admin/event-helper/update-helper');
+
+        // Select Carplate
+        const carPlateDropdown = await page.locator("[data-testid='carplate']");
+        await carPlateDropdown.click(); 
+        await page.waitForSelector('ul[role="listbox"] li button');
+        await page.locator('ul[role="listbox"] li button:has-text("Yellow 5")').click(); 
+
+        //Select Shift
+        const shiftRadio = await page.locator("[data-testid='PM']");
+        await shiftRadio.click();
+        await expect(shiftRadio).toBeChecked();
+
+        // Submit Form
+        const submitButton = await page.locator('button[type="submit"]');
+        await submitButton.click();
+        console.log('Update helpers successfully.');
+
+    });
+});
--- frontend/src/routes/(admin)/admin/event-helper/+page.svelte
+++ frontend/src/routes/(admin)/admin/event-helper/+page.svelte
@@ -168,7 +168,7 @@
                             <td class="px-6 py-4 whitespace-nowrap">{helper.Shift ? 'AM' : 'PM'}</td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
                                 <div class="flex items-center">
-                                    <a href={`event-helper/update-helper/${encodeURIComponent(JSON.stringify(helper))}`} class="text-stone-500 hover:text-green-500 mr-8">
+                                    <a href={`event-helper/update-helper/${encodeURIComponent(JSON.stringify(helper))}`} class="text-stone-500 hover:text-green-500 mr-8" data-testid="update-helper">
                                         <ToolTip text="Update Event Helper"> 
                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                             <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
