af7e7c7ad98f34c7703eb46a5dbe2efbd17de227
Karl <karlorjalo@gmail.com>
2024-08-05 10:49:15 +0800

    debug: Remove logs and alerts

--- frontend/src/lib/components/EventTracker.svelte
+++ frontend/src/lib/components/EventTracker.svelte
@@ -120,10 +120,7 @@
       let nextDist = haversineDistance([nextStop.Lat, nextStop.Lng], [position.coords.latitude, position.coords.longitude])
       let radius = 15
       let margin = 40
-      alert(`current.EventId: ${current.EventId}, currentDist: ${currentDist}, nextDist: ${nextDist}, currentStop: ${currentStop.StopName}, nextStop: ${nextStop.StopName}`)
       if ((current.EventId == 2 && currentDist > (radius + margin)) || (current.EventId == 3 && nextDist < radius) || (current.EventId == 1 && currentDist < radius)) {
-        console.log(`current.EventId: ${current.EventId}, currentDist: ${currentDist}, nextDist: ${nextDist}, currentStop: ${currentStop.StopName}, nextStop: ${nextStop.StopName}`)
-        alert(`current.EventId: ${current.EventId}, currentDist: ${currentDist}, nextDist: ${nextDist}, currentStop: ${currentStop.StopName}, nextStop: ${nextStop.StopName}`)
         createEvent()
       }
 		})
